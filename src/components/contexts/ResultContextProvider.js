d6182ed1c85a7d508053adf9219f1af76e14648f
Angelo Guiao <angeloguiao.gci@gmail.com>
2023-11-13 13:31:38 +0800

    initial commit

--- src/components/contexts/ResultContextProvider.js
+++ src/components/contexts/ResultContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'React';
+import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search74.p.rapidapi.com/';
@@ -6,31 +6,43 @@ const baseUrl = 'https://google-search74.p.rapidapi.com/';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState ('');
+    const [searchTerm, setSearchTerm] = useState('');
 
     const getResults = async (type) => {
         setIsLoading(true);
-
-        const response = await fetch('${baseUrl}${type}', {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'cc8e73a12fmsh9e9f310b5f6938fp14eccdjsnbc67905d8d26',
-                'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+    
+        try {
+            const response = await fetch(`${baseUrl}${type}`, {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': 'cc8e73a12fmsh9e9f310b5f6938fp14eccdjsnbc67905d8d26',
+                    'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+                }
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
-        });
+    
+            const data = await response.json();
+            console.log(data);
+    
+            // Adjust the property based on the actual structure of the API response
+            setResults(data?.results || []);
+    
+        } catch (error) {
+            console.error('Error fetching results:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    
 
-        const data = await response.json();
-
-        setResults(data);
-        setIsLoading(false);
-    }
-
-    return{
-        <ResultContext.Provider value={{ getResults, results, searchTerm, isLoading, setSearchTerm}}>
-        {children}
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, isLoading, setSearchTerm }}>
+            {children}
         </ResultContext.Provider>
-    }
-
-}
+    );
+};
 
 export const useResultContext = () => useContext(ResultContext);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import { ResultContextProvider  } from './components/contexts/ResultContextProvider';
 import './styles.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+    <ResultContextProvider>
 <Router>
 <App />
 </Router>
+</ResultContextProvider>
 
 );
